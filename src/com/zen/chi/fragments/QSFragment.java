f64bb6ca6f3c6ea71fa06053a7577e9edbd0f90d
calebcabob <calphonic@gmail.com>
2019-04-05 15:11:25 -0400

    Chi: Introduce QS tile style picker [2/3]

--- src/com/zen/chi/fragments/QSFragment.java
+++ src/com/zen/chi/fragments/QSFragment.java
@@ -16,14 +16,20 @@
 
 package com.zen.chi.fragments;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.os.UserHandle;
+import android.content.ContentResolver;
 import android.content.Context;
+import android.content.Intent;
 import android.preference.Preference.OnPreferenceChangeListener;
 import android.provider.SearchIndexableResource;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceCategory;
+import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.PreferenceScreen;
 import android.support.v14.preference.PreferenceFragment;
+import android.provider.Settings;
 
 import com.android.settings.R;
 import com.android.internal.logging.nano.MetricsProto;
@@ -37,16 +43,44 @@ import java.util.List;
 public class QSFragment extends SettingsPreferenceFragment
         implements Preference.OnPreferenceChangeListener, Indexable {
 
-    @Override
-    public boolean onPreferenceChange(Preference preference, Object newValue) {
-        return false;
-    }
+    private static final String QS_TILE_STYLE = "qs_tile_style";
+
+    private ListPreference mQsTileStyle;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.qs);
+
+        PreferenceScreen prefScreen = getPreferenceScreen();
+	ContentResolver resolver = getActivity().getContentResolver();
+
+       // QS Styles
+       mQsTileStyle = (ListPreference) findPreference(QS_TILE_STYLE);
+       int qsTileStyle = Settings.System.getIntForUser(resolver,
+               Settings.System.QS_TILE_STYLE, 0,
+	       UserHandle.USER_CURRENT);
+       int valueIndex = mQsTileStyle.findIndexOfValue(String.valueOf(qsTileStyle));
+       mQsTileStyle.setValueIndex(valueIndex >= 0 ? valueIndex : 0);
+       mQsTileStyle.setSummary(mQsTileStyle.getEntry());
+       mQsTileStyle.setOnPreferenceChangeListener(this);
+     }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        ContentResolver resolver = getActivity().getContentResolver();
+
+        if (preference == mQsTileStyle) {
+            int style = Integer.valueOf((String) newValue);
+            int index = mQsTileStyle.findIndexOfValue((String) newValue);
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.QS_TILE_STYLE, style);
+            mQsTileStyle.setSummary(mQsTileStyle.getEntries()[index]);
+            return true;
+        }
+    return false;
     }
+
 
     @Override
     public void onResume() {
