46541452d7c4313330beffdb35a35afb849e5ccd
maxwen <max.weninger@gmail.com>
2019-06-05 22:58:48 -0400

    Chi: OP gesture tweaks[2/2]

--- src/com/zen/chi/fragments/GestureSettings.java
+++ src/com/zen/chi/fragments/GestureSettings.java
@@ -27,6 +27,10 @@ import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceScreen;
 import android.provider.Settings;
 import android.provider.SearchIndexableResource;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
+
+import com.zen.chi.preferences.CustomSeekBarPreference;
 
 import com.android.settings.R;
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
@@ -36,8 +40,15 @@ import com.android.settings.search.Indexable;
 import java.util.List;
 import java.util.Arrays;
 
-public class GestureSettings extends SettingsPreferenceFragment implements Indexable {
+public class GestureSettings extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener, Indexable {
+
     private static final String TAG = "GestureSettings";
+    private static final String KEY_SWIPE_LENGTH = "gesture_swipe_length";
+    private static final String KEY_SWIPE_TIMEOUT = "gesture_swipe_timeout";
+
+    private CustomSeekBarPreference mSwipeTriggerLength;
+    private CustomSeekBarPreference mSwipeTriggerTimeout;
 
     @Override
     public int getMetricsCategory() {
@@ -46,8 +57,43 @@ public class GestureSettings extends SettingsPreferenceFragment implements Index
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         addPreferencesFromResource(R.xml.gesture);
+	ContentResolver resolver = getActivity().getContentResolver();
         mFooterPreferenceMixin.createFooterPreference().setTitle(R.string.gesture_settings_info);
+
+	mSwipeTriggerLength = (CustomSeekBarPreference) findPreference(KEY_SWIPE_LENGTH);
+        int triggerLength = Settings.System.getInt(resolver, Settings.System.BOTTOM_GESTURE_SWIPE_LIMIT,
+                getSwipeLengthInPixel(getResources().getInteger(com.android.internal.R.integer.nav_gesture_swipe_min_length)));
+        mSwipeTriggerLength.setValue(triggerLength);
+        mSwipeTriggerLength.setOnPreferenceChangeListener(this);
+
+        mSwipeTriggerTimeout = (CustomSeekBarPreference) findPreference(KEY_SWIPE_TIMEOUT);
+        int triggerTimeout = Settings.System.getInt(resolver, Settings.System.BOTTOM_GESTURE_TRIGGER_TIMEOUT,
+                getResources().getInteger(com.android.internal.R.integer.nav_gesture_swipe_timout));
+        mSwipeTriggerTimeout.setValue(triggerTimeout);
+        mSwipeTriggerTimeout.setOnPreferenceChangeListener(this);
+    }
+
+    private int getSwipeLengthInPixel(int value) {
+        return Math.round(value * getResources().getDisplayMetrics().density);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        ContentResolver resolver = getActivity().getContentResolver();
+        if (preference == mSwipeTriggerLength) {
+            int value = (Integer) newValue;
+            Settings.System.putInt(resolver,
+                    Settings.System.BOTTOM_GESTURE_SWIPE_LIMIT, value);
+            return true;
+        } else if (preference == mSwipeTriggerTimeout) {
+            int value = (Integer) newValue;
+            Settings.System.putInt(resolver,
+                    Settings.System.BOTTOM_GESTURE_TRIGGER_TIMEOUT, value);
+            return true;
+        }
+        return false;
     }
 
     @Override
